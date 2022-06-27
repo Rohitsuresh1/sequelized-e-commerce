7da61f6c3f24be052c83cdbc966c61e0d8f7b1e5
Rohitsuresh1 <99383088+Rohitsuresh1@users.noreply.github.com>
2022-06-27 00:24:10 -0400

    updated all the models except for the static function and index.js

--- models/Product.js
+++ models/Product.js
@@ -2,14 +2,47 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const { DECIMAL } = require('sequelize/lib/data-types');
+const dataTypes = require('sequelize/lib/data-types');
+const { INTEGER } = require('sequelize');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model {
+
+
+}
 
 // set up fields and rules for Product model
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement:true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(6,2),
+      allowNull:false,
+      validate: DECIMAL
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      defaultValue:10
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model:"category",
+        key:"id"
+      }
+    }
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      tyoe: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey:true,
+      autoIncrement: true
+    },
+    Product_id: {
+      tyoe: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id"
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
